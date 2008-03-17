18f9fd944cae6d85b5eb1b07816c74256b39aa32
Pino Toscano <pino@kde.org>
2008-03-17 13:54:54 +0000

    SVN_SILENT no ' in #warning
    svn path=/trunk/kdereview/step/; revision=786624

--- step/worldgraphics.cc
+++ step/worldgraphics.cc
@@ -72,7 +72,7 @@ bool ItemCreator::sceneEvent(QEvent* event)
         _worldModel->beginMacro(i18n("Create %1", _worldModel->newItemName(_className)));
         _item = _worldModel->newItem(_className); Q_ASSERT(_item != NULL);
 #ifdef __GNUC__
-#warning Don't add item until it is fully created
+#warning Do not add item until it is fully created
 #endif
         const StepCore::MetaProperty* property = _item->metaObject()->property("position");
         if(property != NULL) {
