1a05a5cec8bf6204b5924266ad1fff238d86b15a
Carsten Niehaus <cniehaus@gmx.de>
2008-04-05 12:29:06 +0000

    More CMake fixes
    svn path=/trunk/KDE/kdeedu/step/; revision=793843

--- StepConfigureChecks.cmake
+++ StepConfigureChecks.cmake
@@ -2,4 +2,8 @@ include(CheckIncludeFiles)
 include(CheckFunctionExists)
 
 macro_optional_find_package(GSL)
+macro_optional_find_package(Qalculate)
 find_package(GMM)
+
+macro_log_feature(QALCULATE_FOUND "Qualculate" "Qualculate is needed by Step" "http://qalculate.sourceforge.net/" FALSE "0.9.5" "")
+macro_log_feature(GSL_FOUND "GSL" "Qualculate is needed by Step" "http://www.gnu.org/software/gsl/" FALSE "1.8" "")
