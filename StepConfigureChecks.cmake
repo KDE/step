a032a84278e015ac28682d184dd1985e4bba813a
Allen Winter <winter@kde.org>
2008-04-05 15:19:18 +0000

    improve the macro_log_feature() messages
    svn path=/trunk/KDE/kdeedu/step/; revision=793893

--- StepConfigureChecks.cmake
+++ StepConfigureChecks.cmake
@@ -5,5 +5,5 @@ macro_optional_find_package(GSL)
 macro_optional_find_package(Qalculate)
 find_package(GMM)
 
-macro_log_feature(QALCULATE_FOUND "Qualculate" "Qualculate is needed by Step" "http://qalculate.sourceforge.net/" FALSE "0.9.5" "")
-macro_log_feature(GSL_FOUND "GSL" "Qualculate is needed by Step" "http://www.gnu.org/software/gsl/" FALSE "1.8" "")
+macro_log_feature(QALCULATE_FOUND "Qualculate" "A multi-purpose desktop calculator" "http://qalculate.sourceforge.net/" FALSE "0.9.5" "Enable unit conversion support in Step")
+macro_log_feature(GSL_FOUND "GSL" "The GNU Scientific Library, a numerical library for C and C++" "http://www.gnu.org/software/gsl/" FALSE "1.8" "Enables GSL-powered features in Step")
