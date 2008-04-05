83a0fdfd3fb670288bb9a7faf708df2015fb9413
Allen Winter <winter@kde.org>
2008-04-05 15:30:43 +0000

    check for GMM at the top-level.  no GMM => no Step
    svn path=/trunk/KDE/kdeedu/step/; revision=793905

--- StepConfigureChecks.cmake
+++ StepConfigureChecks.cmake
@@ -3,7 +3,6 @@ include(CheckFunctionExists)
 
 macro_optional_find_package(GSL)
 macro_optional_find_package(Qalculate)
-find_package(GMM)
 
 macro_log_feature(QALCULATE_FOUND "Qualculate" "A multi-purpose desktop calculator" "http://qalculate.sourceforge.net/" FALSE "0.9.5" "Enable unit conversion support in Step")
 macro_log_feature(GSL_FOUND "GSL" "The GNU Scientific Library, a numerical library for C and C++" "http://www.gnu.org/software/gsl/" FALSE "1.8" "Enables GSL-powered features in Step")
