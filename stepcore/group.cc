cf405fb4c9b83c3acbe32a9ea633db93cb78f98d
Aliona Kuznetsova <aliona.kuz@gmail.com>
2008-08-01 17:20:26 +0000

    proportional changing of size
    svn path=/branches/work/soc-stepgame/step/; revision=840741

--- stepcore/group.cc
+++ stepcore/group.cc
@@ -28,7 +28,7 @@ STEPCORE_META_OBJECT(Group, "Group of bodies", 0,
         STEPCORE_SUPER_CLASS(ItemGroup),
         STEPCORE_PROPERTY_RW_D(StepCore::Vector2d, position, "m", "position", position, setPosition)
         STEPCORE_PROPERTY_RW_D(StepCore::Vector2d, velocity, "m/s", "velocity", velocity, setVelocity)
-        STEPCORE_PROPERTY_R_D(StepCore::Vector2d, size, "m", "velocity", size))
+        STEPCORE_PROPERTY_RW_D(StepCore::Vector2d, size, "m", "velocity", size, setSize))
 
 Group::Group(QString name)
 {
@@ -111,7 +111,7 @@ Vector2d Group::size() const{
         }else if(propertyP){
             point = propertyP->readVariant(*it).value<StepCore::Vector2d>();
             point1 = propertyP->readVariant(*it).value<StepCore::Vector2d>();
-        }else break;
+        }else continue;
         
         if(right < point[0]) right = point[0];
         if(left > point1[0]) left = point1[0];
@@ -119,6 +119,58 @@ Vector2d Group::size() const{
         if(bottom > point1[1]) bottom = point1[1];
     }
     return Vector2d(right-left, top-bottom);
+}
+
+void Group::setSize(const Vector2d& size){
+
+    Vector2d initSize = this->size();
+    Vector2d propSize = size;
+    propSize[0] = size[0];
+    propSize[1] = size[0]*initSize[1]/initSize[0];
+    ItemList::const_iterator end = items().end();
+    Vector2d groupPosition = this->position();
+    if(initSize[0] && initSize[1]){
+        for(ItemList::const_iterator it = items().begin(); it != end; ++it) {
+            const StepCore::MetaProperty* propertyP = (*it)->metaObject()->property("position");
+            const StepCore::MetaProperty* propertyS = (*it)->metaObject()->property("size");
+            if(propertyP){
+                Vector2d position = propertyP->readVariant(*it).value<StepCore::Vector2d>();
+                Vector2d newPosition;
+                newPosition[0] = (position[0] - groupPosition[0] )*propSize[0]/initSize[0] + groupPosition[0]; 
+                newPosition[1] = (position[1] - groupPosition[1] )*propSize[1]/initSize[1] + groupPosition[1];
+                propertyP->writeVariant(*it, QVariant::fromValue(newPosition));
+            };
+            if(propertyS){
+                Vector2d itemSize = propertyS->readVariant(*it).value<StepCore::Vector2d>();
+                Vector2d newSize;
+                newSize[0] = itemSize[0]*propSize[0]/initSize[0]; 
+                newSize[1] = newSize[0]*initSize[1]/initSize[0];
+                propertyS->writeVariant(*it, QVariant::fromValue(newSize));
+            };
+        }
+    }else{
+        for(ItemList::const_iterator it = items().begin(); it != end; ++it) {
+            const StepCore::MetaProperty* propertyP = (*it)->metaObject()->property("position");
+            const StepCore::MetaProperty* propertyS = (*it)->metaObject()->property("size");
+            if(propertyP){
+                Vector2d position = propertyP->readVariant(*it).value<StepCore::Vector2d>();
+                Vector2d newPosition;
+                newPosition[0] = propSize[0]/2 + groupPosition[0]; 
+                newPosition[1] = propSize[1]/2 + groupPosition[1];
+                propertyP->writeVariant(*it, QVariant::fromValue(newPosition));
+            };
+            if(propertyS){
+                Vector2d itemSize = propertyS->readVariant(*it).value<StepCore::Vector2d>();
+                Vector2d newSize;
+                if(!itemSize[0]||!itemSize[1]){
+                    newSize[0] = 0;
+                    newSize[1] = 0;
+                }
+                propertyS->writeVariant(*it, QVariant::fromValue(newSize));
+            };
+        }
+        
+    }
 }
 
 } // namespace StepCore
