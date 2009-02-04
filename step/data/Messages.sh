388b1b7dd5f4f50266d2d2b564bf88441eba10bd
Vladimir Kuznetsov <ks.vladimir@gmail.com>
2009-02-04 20:23:58 +0000

    xgettext invocation is temporary commented in Messages.sh
    svn path=/trunk/KDE/kdeedu/step/; revision=921338

--- step/data/Messages.sh
+++ step/data/Messages.sh
@@ -8,8 +8,9 @@ EXTRACTXML_STEP="${EXTRACTXML} --tag=name --tag=text --unquote"
 $EXTRACTXML_HTML --extract ./objinfo/*.html --output=objinfo.cpp
 $EXTRACTXML_STEP --extract ./examples/*.step ./tutorials/*.step --output=examples.cpp
 
-$XGETTEXT objinfo.cpp -o $podir/step_objinfo_files.pot
-$XGETTEXT examples.cpp -o $podir/step_example_files.pot
+# Temporary commented-out
+#$XGETTEXT objinfo.cpp -o $podir/step_objinfo_files.pot
+#$XGETTEXT examples.cpp -o $podir/step_example_files.pot
 
 rm -f objinfo.cpp
 rm -f examples.cpp
