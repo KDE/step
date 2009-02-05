ee31fc8ec2c684d4e75a546ef77e3bfd0a99991d
Vladimir Kuznetsov <ks.vladimir@gmail.com>
2009-02-05 13:20:23 +0000

    More options for adjusting context.
    svn path=/trunk/KDE/kdeedu/step/; revision=921664

--- step/data/Messages.sh
+++ step/data/Messages.sh
@@ -2,16 +2,16 @@
 
 EXTRACTXML="./extractxml"
 
-EXTRACTXML_HTML="${EXTRACTXML} --tag-regex=. --recursive --strip"
+EXTRACTXML_HTML="${EXTRACTXML} --context=%(filename)s --tag-regex=^(?:html|head|title|body|p)$ --recursive --strip"
 EXTRACTXML_STEP="${EXTRACTXML} --tag=name --tag=text --unquote"
 
 $EXTRACTXML_HTML --extract ./objinfo/*.html --output=objinfo.cpp
 $EXTRACTXML_STEP --extract ./examples/*.step ./tutorials/*.step --output=examples.cpp
 
 # Temporary commented-out
-#$XGETTEXT objinfo.cpp -o $podir/step_objinfo_files.pot
-#$XGETTEXT examples.cpp -o $podir/step_example_files.pot
+$XGETTEXT objinfo.cpp -o $podir/step_objinfo_files.pot
+$XGETTEXT examples.cpp -o $podir/step_example_files.pot
 
-rm -f objinfo.cpp
-rm -f examples.cpp
+#rm -f objinfo.cpp
+#rm -f examples.cpp
 
