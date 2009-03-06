a1718bd9adfde27c75197470d2b516f4d9fdfd9b
Laurent Montel <montel@kde.org>
2009-03-06 12:14:41 +0000

    fix extract messages
    svn path=/trunk/KDE/kdeedu/step/; revision=935849

--- Messages.sh
+++ Messages.sh
@@ -1,4 +1,6 @@
 #! /bin/sh
 $EXTRACTRC `find . -name '*.rc'` >> rc.cpp || exit 11
 $EXTRACTRC `find . -name '*.ui'` >> rc.cpp || exit 12
+$EXTRACTRC `find . -name '*.kcfg'` >> rc.cpp || exit 13
 $XGETTEXT `find step -name '*.cc'` rc.cpp -o $podir/step.pot
+rm -f rc.cpp
