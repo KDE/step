6aceb4132d7e549010ab1b715fc71f29dfa7a50b
Sebastian Voecking <kde@voecking.net>
2011-04-07 11:28:20 +0200

    Don't antialias the whole arrow any more, only the head

--- step/arrow.cc
+++ step/arrow.cc
@@ -18,7 +18,11 @@
 
 #include "arrow.h"
 
+#include <cmath>
+
 #include <QPainter>
+
+const int ARROW_STROKE = 6;
 
 Arrow::Arrow(const QLineF& line, int width) : QLineF(line), _width(width)
 {
@@ -38,23 +42,31 @@ void Arrow::draw(QPainter* painter) const
 {
     const QTransform& transform = painter->worldTransform();
     QLineF transformed = transform.map(*this);
-    QLineF line_unit = transformed.unitVector();
+    QLineF real_transformed(floor(transformed.x1()) + 0.5,
+                            floor(transformed.y1()) + 0.5,
+                            floor(transformed.x2()) + 0.5,
+                            floor(transformed.y2()) + 0.5);
+    QLineF line_unit = real_transformed.unitVector();
     line_unit.translate(-line_unit.p1());
     QLineF line_normal = line_unit.normalVector();
     
-    QPointF arrow_head_base = transformed.p2() - line_unit.p2() * 2 * _width;
-    QLineF arrow_line(transformed.p1(), arrow_head_base);
+    QPointF arrow_head_base =
+        real_transformed.p2() - line_unit.p2() * 0.866 * _width;
+    QLineF arrow_line(transformed);
+    arrow_line.setLength(arrow_line.length() - 0.5 * _width);
     
     QPolygonF arrow_head(3);
-    arrow_head[0] = transformed.p2();
-    arrow_head[1] = arrow_head_base + line_normal.p2() * _width / 2.;
-    arrow_head[2] = arrow_head_base - line_normal.p2() * _width / 2.;
+    arrow_head[0] = real_transformed.p2();
+    arrow_head[1] = arrow_head_base + line_normal.p2() * 0.5 * _width;
+    arrow_head[2] = arrow_head_base - line_normal.p2() * 0.5 * _width;
     
     painter->save();
-    painter->setRenderHint(QPainter::Antialiasing, true);
     painter->setWorldMatrixEnabled(false);
-    painter->drawLine(arrow_line);
     QPen pen = painter->pen();
+    pen.setWidth(0);
+    painter->setPen(pen);
+    painter->drawLine(arrow_line);
+    painter->setRenderHint(QPainter::Antialiasing, true);
     painter->setBrush(pen.color());
     painter->setPen(Qt::NoPen);
     painter->drawPolygon(arrow_head);
