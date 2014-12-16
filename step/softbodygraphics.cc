fe84b86417a1781da65a0ed842a232d671c49a76
Inge Wallin <inge@lysator.liu.se>
2014-12-16 08:41:50 -0500

    KF5 port: KDialog --> QDialog for soft bodies.

--- step/softbodygraphics.cc
+++ step/softbodygraphics.cc
@@ -1,5 +1,6 @@
 /* This file is part of Step.
    Copyright (C) 2007 Vladimir Kuznetsov <ks.vladimir@gmail.com>
+   Copyright (C) 2014 Inge Wallin        <inge@lysator.liu.se>
 
    Step is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
