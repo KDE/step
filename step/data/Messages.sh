40735c939fde6e822fb4f72515b0ec6ab8579e50
Pino Toscano <pino@kde.org>
2020-06-05 09:56:42 +0200

    i18n: use dummy source for objinfo and example catalogs
    use rc.cpp as dummy source for the objinfo and example catalogs, so
    repack-pot can properly identify and remove the references to the
    sources
    
    there is no change to the resulting catalogs, only dropped source
    references to the dummy objinfo.cpp and examples.cpp

--- step/data/Messages.sh
+++ step/data/Messages.sh
@@ -2,17 +2,17 @@
 
 EXTRACTXML="./extractxml"
 
+rm -f rc.cpp
 $EXTRACTXML --extract \
         --context='%(filename)s:%(tag)s' --tag-regex='^(?:title|body|p|h[1-6])$' --recursive --strip \
-        ./objectinfo/*.html --output=objinfo.cpp
+        ./objectinfo/*.html --output=rc.cpp
+$XGETTEXT rc.cpp -o $podir/step_objinfo_files.pot
+
+rm -f rc.cpp
 $EXTRACTXML --extract \
         --context='%(tag)s' --tag=name --tag=text --unquote \
         --parse-unquoted='--context=HTML:%(tag)s --tag-regex=^(?:title|body|p|h[1-6])$ --recursive --strip' \
-        ./examples/*.step ./tutorials/*.step --output=examples.cpp
+        ./examples/*.step ./tutorials/*.step --output=rc.cpp
+$XGETTEXT rc.cpp -o $podir/step_example_files.pot
 
-$XGETTEXT objinfo.cpp -o $podir/step_objinfo_files.pot
-$XGETTEXT examples.cpp -o $podir/step_example_files.pot
-
-rm -f objinfo.cpp
-rm -f examples.cpp
-
+rm -f rc.cpp
